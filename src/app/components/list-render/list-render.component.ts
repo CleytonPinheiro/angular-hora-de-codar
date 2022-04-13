33ef34e67f5e4555498d0e99052d15ef59fdde9b
Cleyton <42612444+CleytonPinheiro@users.noreply.github.com>
2022-04-13 16:40:54 -0300

    diretivas, pipes ...

---
+++ src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/Animal';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css'],
+})
+export class ListRenderComponent implements OnInit {
+  animals: Animal[] = [
+    { name: 'Turca', type: 'Dog', age: 10 },
+    { name: 'Tom', type: 'Cat', age: 5 },
+    { name: 'Frida', type: 'Dog', age: 1 },
+    { name: 'Bob', type: 'Horse', age: 3 },
+  ];
+
+  animalDetails = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos.`;
+  }
+}
