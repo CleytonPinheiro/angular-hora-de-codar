aa51e6d21661dd550ee66e65700e3ce6966b17e1
Cleyton <42612444+CleytonPinheiro@users.noreply.github.com>
2022-05-05 11:54:25 -0300

    Buscando dados da api e exibindo

--- moments/src/app/componets/pages/home/home.component.ts
+++ moments/src/app/componets/pages/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MommentService } from 'src/app/services/momment.service';
+import { Moment } from 'src/app/Moment';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
+
+  constructor(private momentService: MommentService) { }
 
   ngOnInit(): void {
-  }
+    this.momentService.getMoments().subscribe((items) => {
 
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+
+    });
+  }
 }
