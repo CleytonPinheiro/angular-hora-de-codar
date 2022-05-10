9e226384c03fea2407106704d52bc3ea0b35b40b
Cleyton <42612444+CleytonPinheiro@users.noreply.github.com>
2022-05-10 20:15:45 -0300

    Exibe dados para editar no form e atualizado no back | aula 39

--- moments/src/app/componets/pages/edit-moment/edit-moment.component.ts
+++ moments/src/app/componets/pages/edit-moment/edit-moment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { Moment } from 'src/app/Moment';
+
+import { MessagesService } from 'src/app/services/messages.service';
 import { MommentService } from 'src/app/services/momment.service';
 
 @Component({
@@ -14,7 +16,9 @@ export class EditMomentComponent implements OnInit {
   btnText: string = 'Editar';
 
   constructor(private momentService: MommentService,
-    private route: ActivatedRoute  
+    private route: ActivatedRoute,
+    private router: Router,
+    private messagService: MessagesService  
   ) { }
 
   ngOnInit(): void {
@@ -25,4 +29,23 @@ export class EditMomentComponent implements OnInit {
     })
   }
 
+  async editHandler(momentData: Moment) {
+   const id = this.moment.id;
+
+   const formData = new FormData();
+
+   formData.append('title', momentData.title);
+   formData.append('description', momentData.description);
+
+   if(momentData.image) {
+    formData.append('image', momentData.image);
+   }
+
+   await this.momentService.updateMoment(id!, formData).subscribe();
+
+   this.messagService.add(`Momento ${id} foi atualizado com sucesso.`);
+
+   this.router.navigate(['/']);
+   
+  }
 }
