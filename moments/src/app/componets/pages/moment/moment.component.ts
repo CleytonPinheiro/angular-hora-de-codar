8f4049820c151ec24cb1c4b0ce0fedc5b834141a
Cleyton <42612444+CleytonPinheiro@users.noreply.github.com>
2022-05-10 16:58:35 -0300

    Estilizando os detalhes

--- moments/src/app/componets/pages/moment/moment.component.ts
+++ moments/src/app/componets/pages/moment/moment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MommentService } from 'src/app/services/momment.service';
 import { Moment } from 'src/app/Moment';
 import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -10,6 +12,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MomentComponent implements OnInit {
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(private momentService: MommentService,private route: ActivatedRoute) { }
 
